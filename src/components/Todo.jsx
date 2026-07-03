d944edca102dfb0b65a6f1ae9ac91c8e4529dda9
Yosef <mercytv932@gmail.com>
2026-07-03 12:37:58 -0400

    Todo App Part 4 Submitting Todo Items

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,10 +1,18 @@
-import { useState } from "react";
+import { use, useState } from "react";
 
 export default function Todo() {
   const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodos([...todos, todo]);
+    setTodo("");
+  }
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           onChange={(e) => setTodo(e.target.value)}
           value={todo}
@@ -12,6 +20,7 @@ export default function Todo() {
         />
         <button type="submit">Add</button>
       </form>
+      {console.log(todos)}
     </div>
   );
 }
