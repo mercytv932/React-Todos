912fe8b50675b4b6e2752c331d14d7c8c298172a
Yosef <mercytv932@gmail.com>
2026-07-04 22:54:30 -0400

    New Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import InlineComponent from "./components/InlineComponent";
 import OutlineComponent from "./components/OutlineComponent";
+import MidlineComponent from "./components/MidlineComponent";
 
 function App() {
   return (
     <div className="App">
       <InlineComponent />
       <OutlineComponent />
+      <MidlineComponent />
     </div>
   );
 }
---
+++ src/components/MidlineComponent.jsx
@@ -0,0 +1,9 @@
+import "../App.css";
+
+export default function MidlineComponent() {
+  return (
+    <div>
+      <h1 className="midline-header">This is the Midline Component</h1>
+    </div>
+  );
+}
